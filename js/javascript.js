e3b9122fd17277527c69b202de41c4d1fbe9029a
unknown <coalcrafter202@gmail.com>
2022-04-02 13:34:25 +0200

    Adds localstorage for selects and changes the way data gets loaded

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,42 @@
-$.getJSON("http://sandbox.gibm.ch/berufe.php", function (data) {
-    data.forEach(element => {
-        $('#job').append(`<option value='${element.beruf_id}'>${element.beruf_name}</option>`)
-    });
-});
-
-$.getJSON(`http://sandbox.gibm.ch/klassen.php`, function (data) {
-    data.forEach(element => {
-        $('#group').append(`<option value='${element.klasse_id}'>${element.klasse_longname}</option>`)
-    });
-});
+$(() => {
+    getJobs();
+    getGroups(localStorage.getItem('job'));
+})
 
 $("#job").change(function () {
-    $.getJSON(`http://sandbox.gibm.ch/klassen.php?beruf_id=${$('#job').val()}`, function (data) {
-        $('#group').html('');
+    getGroups($('#job').val())
+    localStorage.setItem('job', $("#job").val());
+    localStorage.setItem('group', $("#group").val());
+    $("#classes").css('display', "block");
+});
+
+$("#group").change(function () {
+    localStorage.setItem('group', $("#group").val());
+});
+
+function getJobs() {
+    $.getJSON("http://sandbox.gibm.ch/berufe.php", function (data) {
         data.forEach(element => {
-            $('#group').append(`<option value='${element.klasse_id}'>${element.klasse_longname}</option>`)
+            let selectedjob = false
+            if (localStorage.getItem('job') == element.beruf_id) {
+                selectedjob = true;
+                $("#classes").css('display', "block");
+            }
+            $('#job').append(`<option value='${element.beruf_id}' ${selectedjob == true ? "selected" : ""}>${element.beruf_name}</option>`)
         });
     });
-});
+};
+
+function getGroups(val) {
+    $.getJSON(`http://sandbox.gibm.ch/klassen.php${val == null ? '' : '?beruf_id=' + val}`, function (data) {
+        $('#group').html('');
+        data.forEach(element => {
+            let selectedGroup = false
+            if (localStorage.getItem('group') == element.klasse_id) {
+                selectedGroup = true;
+            }
+            $('#group').append(`<option value='${element.klasse_id}' ${selectedGroup == true ? "selected" : ""}>${element.klasse_longname}</option>`)
+        });
+    });
+};
+
