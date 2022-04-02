9808c23de54fda9765172ffedbf52f7c403b9f32
CoalPlays <coalcrafter202@gmail.com>
2022-04-02 15:18:16 +0200

    Adds Table with localstorrage

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,14 @@
+const days = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+let date = moment();
+
 $(() => {
     getJobs();
     getGroups(localStorage.getItem('job'));
+    if(localStorage.getItem('group') != null) {
+        getTable(localStorage.getItem('group'))
+        $("#schedule").css('display', "block");
+        $("#calendarWeek").css('display', "block");    
+    }
 })
 
 $("#job").change(function () {
@@ -12,6 +20,19 @@ $("#job").change(function () {
 
 $("#group").change(function () {
     localStorage.setItem('group', $("#group").val());
+    getTable($("#group").val());
+    $("#schedule").css('display', "block");
+    $("#calendarWeek").css('display', "block");
+});
+
+$("#add").click(function () {
+    addWeek();
+    getTable( $("#group").val());
+});
+
+$("#subtract").click(function () {
+    subtractWeek();
+    getTable( $("#group").val());
 });
 
 function getJobs() {
@@ -30,6 +51,7 @@ function getJobs() {
 function getGroups(val) {
     $.getJSON(`http://sandbox.gibm.ch/klassen.php${val == null ? '' : '?beruf_id=' + val}`, function (data) {
         $('#group').html('');
+        $('#group').html('<option>Wähle deine Klasse aus</option>');
         data.forEach(element => {
             let selectedGroup = false
             if (localStorage.getItem('group') == element.klasse_id) {
@@ -40,3 +62,38 @@ function getGroups(val) {
     });
 };
 
+function getTable(val) {
+    const week = `${getCalendarWeek(date)}-${getYear(date)}`
+    $("#date").html(`${getCalendarWeek(date)}-${getYear(date)}`)
+    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${val}&woche=${week}`, function (data) {
+        $('#tableBody').html('');
+        data.forEach(element => {
+            $('#tableBody').append(`
+            <tr>
+                <td>${moment(element.tafel_datum).format('DD.MM.YYYY')}</td>
+                <td>${days[element.tafel_wochentag]}</td>
+                <td>${moment(element.tafel_von,'h:mm').format('HH:mm')}</td>
+                <td>${moment(element.tafel_bis,'h:mm').format('HH:mm')}</td>
+                <td>${element.tafel_lehrer}</td>
+                <td>${element.tafel_longfach}</td>
+                <td>${element.tafel_raum}</td>
+            </tr>`)
+        });
+    });
+};
+
+function getCalendarWeek(date) {
+    return calendarWeek = moment(date, "DD-MM-YYYY").isoWeek();
+};
+
+function getYear(date) {
+    return date.year();
+}
+
+function addWeek() {
+    date.add(1, "w");
+}
+
+function subtractWeek() {
+    date = date.subtract(1, "w")
+}
